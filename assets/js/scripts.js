f2b86f59fed010b3ceab5780cb7f35b3dcba1e29
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-08-30 11:57:46 +0600

    update (Empty data issue )

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,18 +2,14 @@
     //initialize element null value of form
     (new App()).makeEmptyElementValue();
 
+    let viewTableBody = document.querySelector('#view-table-body');
 
     //add observer
     // Create an observer instance linked to the callback function
     const observer = new MutationObserver(function (mutationsList, observer) {
-        console.log(mutationsList)
-        console.log(observer)
         // Use traditional 'for loops' for IE 11
         for(const mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                console.log(mutation.target)
-                console.log('all childnodes is')
-                console.log(mutation.target.childNodes)
                 if (mutation.target.childNodes.length === 1){
                     // I can use a function to collaborate these item once
                     let tr = document.createElement("tr")
@@ -41,25 +37,34 @@
     });
 
     // Start observing the target node for configured mutations
-    observer.observe(
-        document.querySelector('#view-table-body'),
-        { childList: true, subtree: true }
-    );
+    observer.observe(viewTableBody, { childList: true, subtree: true });
+
+
+    if (viewTableBody.childElementCount=== 0){
+        // I can use a function to collaborate these item once
+        let tr = document.createElement("tr")
+        let td = document.createElement("td")
+        td.setAttribute('colspan', 5)
+        td.innerText = 'No data exists';
+        tr.appendChild(td);
+        viewTableBody.appendChild(tr)
+    }
 
 })();
 
 
-let bookList = [new Book('PHP', 'PHP owner', 'ISB12134'),
-    new Book('JS', 'JS owner', 'ISB12135')
-];
+
+// let bookList = [new Book('PHP', 'PHP owner', 'ISB12134'),
+//     new Book('JS', 'JS owner', 'ISB12135')
+// ];
 
 
-if (bookList.length > 0) {
-    bookList.forEach(function (book, sn) {
-        let html = `<tr data-id="${book.isbn}"><td>${++sn}</td><td>${book.name}</td><td>${book.writer}</td><td>${book.isbn}</td><td><button id="edit" data-name="${book.name}" data-writer="${book.writer}" data-isbn="${book.isbn}">Edit</button><button id="delete" data-isbn="${book.isbn}">Delete</button></td></tr>`;
-        document.querySelector('#view-table-body').innerHTML += html;
-    })
-}
+// if (bookList.length > 0) {
+//     bookList.forEach(function (book, sn) {
+//         let html = `<tr data-id="${book.isbn}"><td>${++sn}</td><td>${book.name}</td><td>${book.writer}</td><td>${book.isbn}</td><td><button id="edit" data-name="${book.name}" data-writer="${book.writer}" data-isbn="${book.isbn}">Edit</button><button id="delete" data-isbn="${book.isbn}">Delete</button></td></tr>`;
+//         document.querySelector('#view-table-body').innerHTML += html;
+//     })
+// }
 
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
